1656989e8e52d2f277b32ed1a9a02bdf54566b36
Alexis Jara <alexis.jara@unl.edu.ec>
2026-05-07 23:19:22 -0500

    commit inicial

---
+++ Backend/src/test/java/com/automatas/automatas/AutomatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automatas.automatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
